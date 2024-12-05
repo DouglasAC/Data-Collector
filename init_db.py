72400a1477a8b59b95c026e5271fa187373c7919
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-12-04 19:27:02 -0600

    Agregar verificación de existencia de tabla antes de crear la base de datos

--- init_db.py
+++ init_db.py
@@ -1,5 +1,8 @@
 from app import app, db
 
 with app.app_context():
-    db.create_all()
-    print("Base de datos inicializada")
+    if db.engine.has_table('data'):
+        print("La tabla 'data' ya existe. No se creó una nueva.")
+    else:
+        db.create_all()
+        print("Base de datos inicializada")
